644e4e782955f4ff4a2289608fd907d84a8e8ac8
a-gbatie <gegejoanis9053@gmail.com>
2019-07-13 20:48:57 -0400

    intial commit

---
+++ Controllors/HomeController.cs
@@ -0,0 +1,13 @@
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+namespace RazorFun {
+    public class HomeController : Controller {
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
